5c97c0fd26a16ba5dd8b69b52d7954910d4d47f8
al-e-dev <alessandrovillogas@gmail.com>
2024-10-23 21:04:19 -0500

    update

--- simisimi.js
+++ simisimi.js
@@ -7,25 +7,27 @@ class Simi {
     }
 
     async simi(query) {
-        const params = new URLSearchParams()
-        params.append('text', query)
-        params.append('lc', 'es')
-        params.append('=', '')
-
-        const { data: result } = await axios({
-            method: 'post',
-            url: this.baseUrl + '/web/simtalk',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                'Accept': 'application/json, text/javascript, /; q=0.01',
-                'X-Requested-With': 'XMLHttpRequest'
-            },
-            data: params
+        return new Promise((resolve, reject) => {
+            const params = new URLSearchParams()
+            params.append('text', query)
+            params.append('lc', 'es')
+            params.append('=', '')
+    
+            const { data: result } = await axios({
+                method: 'post',
+                url: this.baseUrl + '/web/simtalk',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'Accept': 'application/json, text/javascript, /; q=0.01',
+                    'X-Requested-With': 'XMLHttpRequest'
+                },
+                data: params
+            })
+    
+            resolve({
+                message: result.success
+            })
         })
-
-        return {
-            message: result.success
-        }
     }
 }
 
--- wallpaper.js
+++ wallpaper.js
@@ -3,95 +3,96 @@ import * as cheerio from 'cheerio';
 
 class Wallpaper {
     constructor() {
-        this.baseUrl = 'https://wallspic.com/es/search/';
+        this.baseUrl = 'https://wallspic.com/es';
+    }
+    
+    async download(url) {
+        return new Promise(async (resolve, reject) => {
+            const { data: html } = await axios.get(url)
+            const $ = cheerio.load(html)
+            
+            const title = $('h1.wallpaper__title').text().trim()
+            const date = $('div.wallpaper__date').text().trim().replace("Descargado ", "")
+            const category = $('span.detailsList__label').eq(0).next('a').text().trim()
+            const resolution = $('li').filter((i, el) => $(el).find('span.detailsList__label').text().trim()).contents().filter(function() {
+                return this.nodeType === 3
+            }).text().trim()
+            const uploader = $('span.detailsList__label').eq(2).next('a').text().trim()
+            const download = $('a.btn_block.wallpaper__download').attr('href')
+
+            const resolutions = []
+            const promises = []
+
+            $('.defPanel__item').each((i, el) => {
+                const type = $(el).find('button').text().trim()
+                $(el).find('.defPanel__list .defPanel__link').each((j, link) => {
+                    const resUrl = $(link).attr('href')
+                    const text = $(link).text().trim()
+                    promises.push(
+                        axios.get(resUrl).then(response => {
+                            const $res = cheerio.load(response.data)
+                            const downloadLink = $res('a.btn_block.wallpaper__download').attr('href')
+                            resolutions.push({
+                                type: type,
+                                url: resUrl,
+                                resolution: text,
+                                download: downloadLink
+                            })
+                        })
+                    )
+                })
+            })
+
+            await Promise.all(promises)
+
+            resolve({
+                title,
+                date,
+                category,
+                resolution,
+                uploader,
+                download,
+                resolutions
+            })
+            
+        })
     }
 
-    async down(url) {
-        const response = await axios.get(url);
-        const $ = cheerio.load(response.data);
-        const data = $('a.btn_block.wallpaper__download').attr('href');
-        return data;
-    }
-
-    async details(url) {
-        const response = await axios.get(url);
-        const $ = cheerio.load(response.data);
-
-        const title = $('h1.wallpaper__title').text().trim();
-        const date = $('div.wallpaper__date').text().trim().replace("Descargado ", "");
-        const category = $('span.detailsList__label').eq(0).next('a').text().trim();
-        const resolution = $('li').filter((i, el) => $(el).find('span.detailsList__label').text().trim()).contents().filter(function() {
-            return this.nodeType === 3;
-        }).text().trim();
-        const uploader = $('span.detailsList__label').eq(2).next('a').text().trim();
-        const download = $('a.btn_block.wallpaper__download').attr('href');
-
-        const resolutions = [];
-        const promises = [];
-        $('.defPanel__item').each((i, el) => {
-            const type = $(el).find('button').text().trim();
-            $(el).find('.defPanel__list .defPanel__link').each((j, link) => {
-                const url = $(link).attr('href');
-                const text = $(link).text().trim();
-                promises.push(
-                    this.down(url).then(download => {
-                        resolutions.push({
-                            type: type,
-                            url: url,
-                            resolution: text,
-                            download: download
-                        });
-                    })
-                );
-            });
-        });
-
-        await Promise.all(promises);
-
-        return {
-            title,
-            date,
-            category,
-            resolution,
-            uploader,
-            download,
-            resolutions
-        };
-    }
-
-    async wall(query) {
-        const response = await axios.get(`${this.baseUrl}${encodeURIComponent(query)}`);
-        const $ = cheerio.load(response.data);
-
-        const results = [];
-        const promises = [];
-
-        $('script').each((i, el) => {
-            const script = $(el).html();
-
-            if (script && script.includes('window.mainAdaptiveGallery')) {
-                const json = script.match(/window.mainAdaptiveGallery\s*=\s*(\{.*?\});/s);
-                if (json) {
-                    const data = JSON.parse(json[1]);
-                    data.list.forEach(item => {
-                        const url = item.original.link;
-                        promises.push(
-                            this.details(url)
-                                .then(details => {
-                                    results.push({
-                                        ...details,
-                                        preview: item.thumbnail.link
-                                    });
-                                })
-                        );
-                    });
+    async search(query) {
+        return new Promise((resolve, reject) => {
+            const { data: html } = await axios.get(`${this.baseUrl}/search/${encodeURIComponent(query)}`);
+            const $ = cheerio.load(html);
+    
+            const results = []
+            const promises = []
+    
+            $('script').each((i, el) => {
+                const script = $(el).html()
+    
+                if (script && script.includes('window.mainAdaptiveGallery')) {
+                    const json = script.match(/window.mainAdaptiveGallery\s*=\s*(\{.*?\});/s)
+                    if (json) {
+                        const data = JSON.parse(json[1])
+                        data.list.forEach(item => {
+                            const url = item.original.link
+                            promises.push(
+                                this.download(url)
+                                    .then(details => {
+                                        results.push({
+                                            ...details,
+                                            preview: item.thumbnail.link
+                                        })
+                                    })
+                            )
+                        })
+                    }
                 }
-            }
-        });
-
-        await Promise.all(promises);
-
-        return results;
+            })
+    
+            await Promise.all(promises)
+    
+            resolve(results)
+        })
     }
 }
 
